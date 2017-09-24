aac6fd56d87001974f056a10086cb3871a4af5de
Eduardo Garcia <eduardo.garcia@esailors.de>
2017-09-24 02:41:56 +0200

    Change subtree push into split and force push

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,4 +2,5 @@
 
 git add --force _site
 git commit -m "Release `date '+%Y-%m-%d %H:%M'`"
-git subtree push --squash --prefix  _site/ origin gh-pages
+git subtree split --prefix _site -b gh-pages
+git push -f origin gh-pages:gh-pages
